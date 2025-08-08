c5320cdf7cceacec6584e0fa5f80e96e195b62a9
Nando <fernanrods1002@gmail.com>
2025-08-08 16:24:21 -0300

    tabuleiro e color

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Position pos = new Position(1, 2);
-        System.out.println(pos);
+        ChessMatch chessMatch  = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
