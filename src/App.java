515fe6ec82bdc1acfeb7ef0a574f39a237aabe13
Nando <fernanrods1002@gmail.com>
2025-08-13 15:36:21 -0300

    movendo peças

--- src/App.java
+++ src/App.java
@@ -1,9 +1,30 @@
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        ChessMatch chessMatch  = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        Scanner sc = new Scanner(System.in);
+
+        ChessMatch chessMatch = new ChessMatch();
+
+        //fica repetindo infinitamente
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println("Destino: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+        }
+
     }
 }
--- src/UI.java
+++ src/UI.java
@@ -1,6 +1,9 @@
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -39,15 +42,29 @@ public class UI {
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
             System.out.print("-");
-        }
-        else {
+        } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
+            } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
+    }
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+
+            String s = sc.nextLine();
+
+            //a1 coulna-linha
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+
+            return new ChessPosition(column, row);
+            
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("Posição inválida");
+        }
     }
 }
